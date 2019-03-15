933bb4895061aa054e0de7d8a4dc81ee48789695
Mathieu Morrissette <mathieumorrissette@gmail.com>
2019-03-14 20:29:40 -0400

    character sheet

---
+++ HttpServer/websites/exalted/templates/Sheet_Template.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer.websites.exalted.templates
+{
+    class Sheet_Template : ITemplate
+    {
+        private User user;
+        
+        public Sheet_Template(User user)
+        {
+            this.user = user;
+        }
+
+        public string Render()
+        {
+            Header_Template headerTemplate = new Header_Template();
+            Footer_Template footerTemplate = new Footer_Template();
+
+            string content = File.ReadAllText(WebSite.WEBSITE_ROOT_PATH + "public/html/sheet.html");
+
+            return headerTemplate.Render() + content + footerTemplate.Render();
+        }
+    }
+}
